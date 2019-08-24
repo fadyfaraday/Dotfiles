616e5ea74b5d035c6358eed973f3d2b784ba0e3a
fady nagh <fadyfaraday@gmail.com>
2019-08-24 16:43:38 -0100

    make some changes to my dotfiles

--- programes.sh
+++ programes.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #These are the programs that I use 
-sudo pacman -S youtube-dl rofi aria2 nmap thunar anki calibre firefox gparted opera redshift wine vim tmux powerline arandr cmus lxappearance telegram-desktop feh youtube-viewer mpv mplayer evince imagewriter termite chromium code compton gedit git hplip htop keepassxc lximage-qt qbittorrent ranger tlp 
+sudo pacman -S youtube-dl rofi aria2 nmap thunar anki calibre firefox gparted opera redshift wine vim tmux powerline arandr cmus lxappearance telegram-desktop feh youtube-viewer mpv mplayer evince imagewriter termite chromium code compton gedit git hplip htop keepassxc lximage-qt qbittorrent ranger tlp exa bat
